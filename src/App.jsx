7fbafd7619f76af966b14abd8ab7130e8656d5ec
Ibukunoluwa Oluwafemi <68595066+ibukun06@users.noreply.github.com>
2026-03-19 14:56:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -683,7 +683,7 @@ function RegisterView({ onRegister, T }) {
   const positions = [
     "Speaker / President","Deputy Speaker / Vice President",
     "Chief Whip","Deputy Chief Whip",
-    "Senate President","Secretary General","Committee Chair",
+    "Senate President","Legislative Secretary / Secretary General","Committee Chair",
     "Financial Secretary","Other Legislative Officer",
     "Honourable Member","Departmental Representative","Student","Staff"
   ];
