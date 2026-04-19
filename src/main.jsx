2194bac5ae4a8ac0cbde13b08fe0cb9408615b41
Ibukunoluwa Oluwafemi <68595066+ibukun06@users.noreply.github.com>
2026-04-19 17:12:53 +0100

    Refactor main.jsx to use dynamic rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Agenda from './Agenda.jsx'
 import CardGenerator from './CardGenerator.jsx'
 
-const isCardPage = window.location.pathname === '/card'
+const path = window.location.pathname
+
+const renderTarget =
+  path === '/agenda' ? <Agenda /> :
+  path === '/card'   ? <CardGenerator /> :
+                       <App />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {isCardPage ? <CardGenerator /> : <App />}
-  </React.StrictMode>
+  <React.StrictMode>{renderTarget}</React.StrictMode>
 )
